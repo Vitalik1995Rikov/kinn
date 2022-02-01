82d0b3eebf89cb2d8835bf8514b3e8e964c8544b
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-02-01 19:24:11 +0300

    clean project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import Header from "./components/Header/Header";
+// import LeftSide from "./components/LeftSide/LeftSide";
+// import CenterSide from "./components/CenterSide/CenterSide";
+// import RightSide from "./components/RightSide/RightSide";
+
+const App = () => {
+  return (
+    <div>
+      1
+      <div>
+        {/* <Header />
+      </div>
+      <div className="flex">
+        <div className="w-1/4">
+          <LeftSide />
+        </div>
+        <div className="w-1/2">
+          <CenterSide />
+        </div>
+        <div className="w-1/4">
+          <RightSide /> */}
+        {/* </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default App;
