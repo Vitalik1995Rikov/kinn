8275ed6387fb618852ec1307a93921559927fa1f
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-02-28 21:34:32 +0300

    flex in SignUp and YourTable

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SignUp = () => {
   return (
-    <div>
-      <h1>Sign up to our newsletter</h1>
-      <span>Receive special offers and look at new products</span>
+    <div className="flex flex-col h-16 p-8 m-9">
+      <h1 className="text-center text-2xl">Sign up to our newsletter</h1>
+      <span className="text-center">Receive special offers and look at new products</span>
       <button>SUBSCRIBE</button>
     </div>
   );
--- src/components/YourTable.js
+++ src/components/YourTable.js
@@ -3,16 +3,18 @@ import React from 'react';
 const YourTable = () => {
   return (
     <div className="flex">
-      <div>
-        <h1>Your Table Your Time</h1>
-        <span className="p-5">
+      <div className="w-3/5 p-10">
+        <div className="flex justify-center text-xl">
+          <h1>Your Table Your Time</h1>
+        </div>
+        <p className="p-5 text-center">
           We believe in thoughtful, international living. So we proLorem ipsum dolor, sit amet
           consectetur adipisicing elit. Doloremque officiis delectus porro odio non facilis
           architecto quisquam id similique rerum, dicta esse dignissimos saepe, odit ratione?
           Dolores minima pariatur at?
-        </span>
+        </p>
       </div>
-      <div>
+      <div className="p-10">
         <img src="../../assets/YourTable.png" alt="img" />
       </div>
     </div>
