f46d6f3269b21d5cb753a78b54d9217a87bc7593
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-02-28 21:17:07 +0300

    changes in OurDesign

--- src/components/OurDesignStory.js
+++ src/components/OurDesignStory.js
@@ -6,9 +6,11 @@ const OurDesignStory = () => {
       <div className="">
         <img src="../../assets/OurDesign.png" alt="img" />
       </div>
-      <div className="flex">
-        <span>Our Design Story</span>
-        <span className="text-xl px-28 pt-24 pb-72">
+      <div className="flex flex-col px-28 pt-24 pb-72 w-3/5">
+        <div>
+          <span className="text-xl">Our Design Story</span>
+        </div>
+        <span>
           We believe in thoughtful, international living. So we proLorem ipsum dolor, sit amet
           consectetur adipisicing elit. Doloremque officiis delectus porro odio non facilis
           architecto quisquam id similique rerum, dicta esse dignissimos saepe, odit ratione?
