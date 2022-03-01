df5f6085e66124510a081ee68fe631546aa1939a
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-03-01 22:17:01 +0300

    text relative - absolute

--- src/components/Essentials.js
+++ src/components/Essentials.js
@@ -1,13 +1,11 @@
 const Essentials = () => {
   return (
-    <div>
-      <div className="z-0">
-        <span className="text-lg relative top-32 left-96 ">THE ESSENTIALS</span>
-        <span className="text-5xl relative top-44 text-tahiti-100">
-          We believe minimalism means more. More space, more time, more meaningful moments.
-        </span>
-      </div>
-      <img className="z-0" src="../../assets/Essentials.png" alt="img" />
+    <div className="bg-amber-100 my-6 px-4 py-4 relative">
+      <span className="text-lg absolute top-32 left-96 ">THE ESSENTIALS</span>
+      <span className="text-5xl absolute top-44 text-tahiti-100">
+        We believe minimalism means more. More space, more time, more meaningful moments.
+      </span>
+      <img src="../../assets/Essentials.png" alt="img" />
     </div>
   );
 };
