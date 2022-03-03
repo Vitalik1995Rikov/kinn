8880e7199b29195d214cd79b5136381bffdeb427
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-03-03 21:48:22 +0300

    text changes

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,9 +4,9 @@ const Image = () => {
   return (
     <div className="relative">
       <div className="absolute bottom-40 right-40">
-        <div>ALL COLLECTIONS</div>
-        <div>ELEVATE YOUR EVERYDAY</div>
-        <button className="">SHOP NOW</button>
+        <div className="text-[#56593D]">ALL COLLECTIONS</div>
+        <div className="text-2xl text-[#56593D]">Elevate Your Everyday</div>
+        <button className="bg-[#56593D] text-[#FFFFEF] py-2 px-4">SHOP NOW</button>
       </div>
       <img className="" src="../../assets/Rectangle.png" alt="img" />
     </div>
